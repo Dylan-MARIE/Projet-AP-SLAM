491c0d4853741fc134b28f7b66ca45873ef62e5c
Dylan-MARIE <dylan.marie@caensup.fr>
2023-12-21 17:22:11 +0100

    Avancées globales

---
+++ backend/formulaire.php
@@ -0,0 +1,34 @@
+<?php
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+
+if (!empty($_POST)){
+    $mail = new PHPMailer(true);
+
+    try {
+        // Configuration du serveur de messagerie
+        //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
+        $mail->isSMTP();
+        $mail->Host = 'smtp.gmail.com';
+        $mail->SMTPAuth = true;
+        $mail->Username = 'dylan.marie.test@gmail.com';
+        $mail->Password = 'test.smtp';
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
+        $mail->Port = 465;
+
+        // Destinataire
+        $mail->setFrom('dylan.marie.test@gmail.com', 'Mailer');
+        $mail->addAddress('dylan.marie.test@gmail.com'); // Remplacez par l'adresse de votre destinataire
+
+        // Contenu du mail
+        $mail->isHTML(true);
+        $mail->Subject = $_POST['objet'] ?? 'Objet';
+        $mail->Body = $_POST['message'] ?? 'This is the HTML message body <b>in bold!</b>';
+
+        $mail->send();
+        echo 'Le message a été envoyé.';
+    } catch (Exception $e) {
+        $error= "Le message n'a pas été envoyé ! Mailer Error: {$mail->ErrorInfo}";
+    }
+}
+?>
---
+++ php/contact.php
@@ -0,0 +1,44 @@
+<?php
+    $data=yaml_parse_file('data/contact.yaml');
+?>
+
+<section id="contact">
+    <h1>Contact</h1>
+    <div class="contenu-contact">
+    <form action="index.php" method="post">
+        <label for="nom">Nom de l'expéditeur :</label>
+        <input type="text" id="nom" name="nom" required>
+
+        <label for="email">Adresse de courriel de l'expéditeur :</label>
+        <input type="email" id="email" name="email" required>
+
+        <label for="objet">Objet du message :</label>
+        <input type="text" id="objet" name="objet" required>
+
+        <label for="message">Contenu du message :</label>
+        <textarea id="message" name="message" required></textarea>
+
+        <label for="captcha">Captcha → 2 + 2 =</label>
+        <input type="number" id="captcha" name="captcha" required>
+
+        <button type="submit">Envoyer le message</button>
+
+        <details class="rgpd">
+        <summary class="rgpd_titre">Règlement Général sur la Protection des Données</summary>
+            Sur ce site, votre vie privée est respectée et le Règlement Général sur la Protection des Données est appliqué. 
+            Les informations que vous fournissez via ce formulaire ne seront utilisées que dans le but spécifié et ne seront pas 
+            conservées au-delà de la période nécessaire pour atteindre cet objectif. // La récupération des données ne sert qu'à l'envoi d'un mail et ces données ne sont pas stockées.
+        </details>
+    </form>
+    </div>
+    <footer>
+        <a class="arrow-color-left" href="#formation">
+            <span class="arrow-color-right">Page précédente</span>
+            <i class="fa-solid fa-arrow-left-long"></i>
+        </a>
+        <a class="arrow-color-right" href="#accueil">
+            <i class="fa-solid fa-arrow-right-long"></i>
+            <span class="arrow-color-right">Page suivante</span>
+        </a>
+    </footer>
+</section>
