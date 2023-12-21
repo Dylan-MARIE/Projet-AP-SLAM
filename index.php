968c6eb03869da7d4d93a80405ffe5240c01e19d
Dylan Marie <dylan.marie@caensup.fr>
2023-12-21 04:48:38 +0100

    Fin du formulaire HTML/CSS // Appliquer backend/contact.php à ce même formulaire // Réorganiser le CSS et raccourcir si possible // Convertir la section CONTACT en php

--- index.php
+++ index.php
@@ -1,16 +1,13 @@
-<?php
-    include("lib/yaml/yaml.php");
-?>
-
 <!doctype html>
 <html lang="fr">
 <head>
-    <title>Site professionnel de Dylan MARIE</title>
     <meta charset="UTF-8">
+    <title>Site professionnel de Dylan MARIE</title>
     <link rel="stylesheet" href="assets/css/site.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Darker+Grotesque&display=swap" rel="stylesheet">
+    <?php include("lib/yaml/yaml.php"); ?>
 </head>
 <body>
     <!--MENU DE NAVIGATION-->
@@ -46,8 +43,33 @@
 <!--SECTION CONTACT-->
     <section id="contact">
         <h1>Contact</h1>
-        <p>Formulaire de contact : Nom de l'expéditeur / Adresse de courriel de l'expéditeur / Objet du message / Contenu du message / Captcha pour éviter les robots / Envoi d'un mail vers mon adresse email</p>
-            
+        <div class="contenu-contact">
+            <form action="backend/contact.php" method="post">
+                <label for="nom">Nom de l'expéditeur :</label>
+                <input type="text" id="nom" name="nom" required>
+
+                <label for="email">Adresse de courriel de l'expéditeur :</label>
+                <input type="email" id="email" name="email" required>
+
+                <label for="objet">Objet du message :</label>
+                <input type="text" id="objet" name="objet" required>
+
+                <label for="message">Contenu du message :</label>
+                <textarea id="message" name="message" required></textarea>
+
+                <label for="captcha">Captcha → 2 + 2 =</label>
+                <input type="number" id="captcha" name="captcha" required>
+
+                <button type="submit">Envoyer le message</button>
+
+                <details class="rgpd">
+                <summary class="rgpd_titre">Règlement Général sur la Protection des Données</summary>
+                    Sur ce site, votre vie privée est respectée et le Règlement Général sur la Protection des Données est appliqué. 
+                    Les informations que vous fournissez via ce formulaire ne seront utilisées que dans le but spécifié et ne seront pas 
+                    conservées au-delà de la période nécessaire pour atteindre cet objectif.
+                </details>
+            </form>
+        </div>
         <footer>
             <a class="arrow-color-left" href="#formation">
                 <span class="arrow-color-right">Page précédente</span>
