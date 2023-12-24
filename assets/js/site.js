12b347c26b582134d3781f82a633e8d71867a1b4
Dylan Marie <dylan.marie@caensup.fr>
2023-12-24 13:57:12 +0100

    test form 3

--- assets/js/site.js
+++ assets/js/site.js
@@ -2,14 +2,11 @@ window.onload = function(){
     window.location.href = "#accueil";
 };
 
-document.getElementById("envoyerBtn").addEventListener("click", function(event) {
-    event.preventDefault();
-
+document.getElementById("contact-form").addEventListener("submit", function(event) {
     var recaptchaResponse = grecaptcha.getResponse();
 
     if (recaptchaResponse.length === 0) {
         alert("Veuillez valider le reCAPTCHA avant d'envoyer le formulaire.");
-    } else {
-        document.getElementById("contact-form").submit();
+        event.preventDefault();
     }
 });
