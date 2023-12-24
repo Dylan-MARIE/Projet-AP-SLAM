8e68a3a5852bf44455fe982c60a643ef73f9e397
Dylan Marie <dylan.marie@caensup.fr>
2023-12-24 15:55:35 +0100

    test form nouvelle méthode 1

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,3 +1,12 @@
 window.onload = function(){
     window.location.href = "#accueil";
 };
+
+function validateForm() {
+    var response = grecaptcha.getResponse();
+    if (response.length == 0) {
+        alert('Veuillez valider le reCAPTCHA avant d\'envoyer le formulaire.');
+        return false;
+    }
+    return true;
+}
--- backend/formulaire.php
+++ backend/formulaire.php
@@ -1,29 +1,42 @@
 <?php
+require_once 'vendor/autoload.php';
+
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\SMTP;
-use ReCaptcha\ReCaptcha;
 
-// Vérification reCAPTCHA
-$recaptcha_secret_key = '6LfqqzopAAAAAImz_I94495ymri7oU0nP4RTvSDy';
-$recaptcha = new ReCaptcha($recaptcha_secret_key);
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    $recaptchaSecretKey = "6LfqqzopAAAAAImz_I94495ymri7oU0nP4RTvSDy";
+    $recaptchaResponse = $_POST["g-recaptcha-response"];
 
-$gRecaptchaResponse = $_POST['g-recaptcha-response'];
-$remoteIp = $_SERVER['REMOTE_ADDR'];
-$resp = $recaptcha->setExpectedHostname('srv1-vm-1137.sts-sio-caen.info')
-                  ->verify($gRecaptchaResponse, $remoteIp);
+    $url = "https://www.google.com/recaptcha/api/siteverify";
+    $data = [
+        "secret" => $recaptchaSecretKey,
+        "response" => $recaptchaResponse,
+    ];
 
-if ($resp->isSuccess()) {
-    // Si la vérification reCAPTCHA est réussie, traiter le reste du formulaire
-    if (!empty($_POST)) {
+    $options = [
+        "http" => [
+            "header" => "Content-type: application/x-www-form-urlencoded\r\n",
+            "method" => "POST",
+            "content" => http_build_query($data),
+        ],
+    ];
+
+    $context = stream_context_create($options);
+    $result = file_get_contents($url, false, $context);
+    $response = json_decode($result, true);
+
+    if ($response["success"] == true) {
         $mail = new PHPMailer(true);
+
         try {
             // Configuration du serveur de messagerie
             //$mail->SMTPDebug = SMTP::DEBUG_SERVER;
             $mail->isSMTP();
             $mail->Host = 'smtp.gmail.com';
             $mail->SMTPAuth = true;
-            $mail->Username = 'dylan.marie.test@gmail.com';
-            $mail->Password = 'test.smtp';
+            $mail->Username = 'dylan.marie.test@gmail.com'; // Remplacez par votre adresse email
+            $mail->Password = 'test.smtp'; // Remplacez par votre mot de passe
             $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;
             $mail->Port = 465;
 
@@ -45,9 +58,8 @@ if ($resp->isSuccess()) {
         if (isset($error)) {
             echo "<div class='erreur'>$error</div>";
         }
+    } else {
+        echo "reCAPTCHA verification failed.";
     }
-} else {
-    $errors = $resp->getErrorCodes();
-    var_dump($errors);
 }
 ?>
