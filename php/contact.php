3d952e99b4be479987a4a706638885ecc98192ae
Dylan Marie <dylan.marie@caensup.fr>
2023-12-25 01:17:13 +0100

    Version pré-finale

--- php/contact.php
+++ php/contact.php
@@ -1,33 +1,34 @@
 <?php
-    $data = yaml_parse_file('data/contact.yaml');
+    $data=yaml_parse_file('data/contact.yaml');
+    $formulaire=$data['formulaire'];
+    $rgpd=$formulaire['rgpd'];
 ?>
 
 <section id="contact">
-    <h1>Contact</h1>
+    <h1><?= $data['titre'] ?></h1>
     <div class="contenu-contact">
     <form action="../index.php" method="post" id="contact-form" onsubmit="return validateForm()">
-        <label for="nom">Nom de l'expéditeur :</label>
+        <label for="nom"><?= $formulaire['nom']; ?></label>
         <input type="text" id="nom" name="nom" required>
 
-        <label for="email">Adresse de courriel de l'expéditeur :</label>
+        <label for="email"><?= $formulaire['email']; ?></label>
         <input type="email" id="email" name="email" required>
 
-        <label for="objet">Objet du message :</label>
+        <label for="objet"><?= $formulaire['objet']; ?></label>
         <input type="text" id="objet" name="objet" required>
 
-        <label for="message">Contenu du message :</label>
+        <label for="message"><?= $formulaire['message']; ?></label>
         <textarea id="message" name="message" required></textarea>
 
         <div class="g-recaptcha" data-sitekey="6LfqqzopAAAAAA1-KHPbW9pJdpsAFKuQVWY9JeT9"></div>
 
-        <button type="submit" id="envoyerBtn">Envoyer le message</button>
+        <button type="submit"><?= $formulaire['bouton']; ?></button>
 
         <details class="rgpd">
-            <summary class="rgpd_titre">Règlement Général sur la Protection des Données</summary>
-            Ce site respecte la vie privée des personnes conformément au Règlement Général sur la Protection des Données (RGPD). 
-            Les données fournies via le formulaire sont utilisées uniquement pour l'envoi d'emails, sans conservation au-delà de l'objectif spécifié.
-            Davantage d'informations concernant le RGPD et le traitement des données personnelles sont disponibles sur le site officiel de la 
-            <a href="https://www.cnil.fr/fr/reglement-europeen-protection-donnees" class="cnil" target="_blank">CNIL</a>.
+            <summary class="rgpd_titre"><?= $formulaire['annexe']; ?></summary>
+            <?= $rgpd['§1']; ?>
+            <a href="<?= $rgpd['lien'] ?>" class="cnil" target="_blank"><?= $rgpd['§2']; ?></a>
+            <?= $rgpd['§3']; ?>
         </details>
     </form>
     </div>
